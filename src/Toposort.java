5e28172c9842ef8fdb94da12e83899d7e73cc2b6
jrdisser <jimdisser@altocirrusapps.com>
2018-11-15 11:34:18 -0700

    added variation of quicksort to toposort algorithm in Toposort

--- src/Toposort.java
+++ src/Toposort.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Toposort {
@@ -8,21 +9,28 @@ public class Toposort {
 	static int m = 0;
 	static int[] pre;
 	static int[] post;
+	static int[] order;
+	static int[] postIndex;
 	static int clock = 1;
 	static ArrayList<ArrayList<Integer>> adj = null;
+	private static Random random = new Random();
 	
 	
     private static ArrayList<Integer> toposort(ArrayList<ArrayList<Integer>> adj) {
-        
+
     	dfs();
-    	
-        ArrayList<Integer> order = new ArrayList<Integer>();
+
+        order = Arrays.copyOf(post, n);
         
-        for(int i = n - 1; n >= 0; --i) {
-        	order.add(post[i]);
+        randomizedQuickTopoSort(order, postIndex, 0, n - 1);
+        
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        
+        for(int e = n - 1; e >= 0; --e) {
+        	result.add(postIndex[e]);
         }
 
-        return order;
+        return result;
     }
 
 	 private static void explore(int x) {
@@ -59,6 +67,54 @@ public class Toposort {
 	    	}
 	    		
 	    }
+	 
+	 private static void randomizedQuickTopoSort(int[] a, int[] ai, int l, int r) {
+	        if (l >= r) {
+	            return;
+	        }
+	        int k = random.nextInt(r - l + 1) + l;
+	        int t = a[l];
+	        a[l] = a[k];
+	        a[k] = t;
+	        
+	        t = ai[l];			//mirror the operations on the index array
+	        ai[l] = ai[k];
+	        ai[k] = t;
+	        
+	        //use partition2
+	        
+	        int m = partition2(a, ai, l, r);
+	        randomizedQuickTopoSort(a, ai, l, m - 1);
+	        randomizedQuickTopoSort(a, ai, m + 1, r);
+	        
+
+	    }
+	 
+	 private static int partition2(int[] a, int[] ai, int l, int r) {
+	        int x = a[l];
+	        int j = l;
+	        for (int i = l + 1; i <= r; i++) {
+	            if (a[i] <= x) {
+	                j++;
+	                int t = a[i];
+	                a[i] = a[j];
+	                a[j] = t;
+	                
+	                t = ai[i];				//mirror the operations on the index array
+	                ai[i] = ai[j];
+	                ai[j] = t;
+	            }
+	        }
+	        int t = a[l];
+	        a[l] = a[j];
+	        a[j] = t;
+	        
+	        t = ai[l];				//mirror the operations on the index array
+            ai[l] = ai[j];
+            ai[j] = t;
+	        return j;
+	    } 
+
 
     public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
@@ -66,10 +122,12 @@ public class Toposort {
         m = scanner.nextInt();
         pre = new int[n];
         post = new int[n];
+        postIndex = new int[n];
         adj = new ArrayList<ArrayList<Integer>>();
         for (int i = 0; i < n; i++) {
 //        	System.out.println("Added v: " + i);
             adj.add(new ArrayList<Integer>());
+            postIndex[i] = i;
         }
         for (int i = 0; i < m; i++) {
             int x, y;
