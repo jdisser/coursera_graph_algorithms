ac9ddb1fa243b4aa549dccaf8ba0c4684cc75736
jrdisser <jimdisser@altocirrusapps.com>
2018-11-27 12:02:09 -0700

    added logging, fixed minSiftUp logic in Dijkstra

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -58,35 +58,52 @@ class Graph {
 	}
 	
 	private void minSiftDown(int i) {
-    	int maxi = i;
+		
+		System.out.println("minSiftDown 1: " + i);
+		printHeap();
+		
+    	int mini = i;
     	int li = heap.size() - 1;	//0 based last index
     	int left = 2*i + 1;
     	int rght = 2*i + 2;
-    	if(left <= li && heap.get(left).dist > heap.get(maxi).dist) {
-    		maxi = left;
+    	if(left <= li && heap.get(left).dist < heap.get(mini).dist) {
+    		mini = left;
     	}
-    	if(rght <= li && heap.get(rght).dist > heap.get(maxi).dist) {
-    		maxi = rght;
+    	if(rght <= li && heap.get(rght).dist < heap.get(mini).dist) {
+    		mini = rght;
     	}
-    	if(maxi != i) {
-	   		swap(i, maxi);
-	   		minSiftDown(maxi);
+    	if(mini != i) {
+	   		swap(i, mini);
+	   		minSiftDown(mini);
     	}
    		
     }
 	
+	private void printHeap() {
+		System.out.print("heap: ");
+		for(Node n : heap) {
+			System.out.print(n.index + ":" + n.dist + " ");
+		}
+		System.out.println(" ");
+	}
+	
+	
+	
 	private void minSiftUp(int i) {
 		
 		int pi;
-		
-		if(i == 0)
-			return;
-		
-		pi = (i - 1)/2;
-		
-		if(heap.get(pi).dist > heap.get(i).dist) {
-			swap(pi, i);
-			minSiftUp(pi);
+		System.out.println(" minSiftUp i: " + i);
+		printHeap();
+		if(i > 0) {
+			pi = (i - 1)/2;
+			
+			System.out.println(" minSiftUp pi: " + pi);
+			
+			if(heap.get(pi).dist > heap.get(i).dist) {
+				swap(pi, i);
+				minSiftUp(pi);
+			}
+			
 		}
 
 	}
@@ -102,6 +119,8 @@ class Graph {
 	}
 	
 	private void decreaseKey(int i, long d) {
+		
+		System.out.println(" decreaseKey i: " + i + " d: " + d);
 		Node dn = map.get(i);
 		dn.dist = d;
 		int dni = heap.indexOf(dn);
@@ -128,6 +147,10 @@ class Graph {
 		while(!heap.isEmpty()) {
 			Node r = getMin();
 			r.visited = true;
+			
+			System.out.println("processing Node: " + r.index);
+			
+			
 			for(Edge e : graph.get(r.index)) {
 				
 				if(r.dist == Long.MAX_VALUE)		//this is an unreachable node if it's dist is infinite
