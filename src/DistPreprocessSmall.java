3c075d96dd9694fbf3f018967698cb42311198b4
jrdisser <jimdisser@altocirrusapps.com>
2018-12-31 17:54:29 -0700

    added biGrpah class to DistPreprocessSmall before refactoring for contraction

--- src/DistPreprocessSmall.java
+++ src/DistPreprocessSmall.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 
 class Node {
@@ -94,6 +96,532 @@ class Edge {
 	
 	
 	
+}
+
+class BiGraph {
+	
+	public ArrayList<ArrayList<Edge>> graph;		//adjacency list of edges
+	public ArrayList<ArrayList<Edge>> graphR;		//adjacency list of reversed edges
+	public ArrayList<Node> heap;					//priority queue with method to update key values
+	public ArrayList<Node> heapR;					//priority queue for reversed graph
+	public ArrayList<Node> preProc;					//priority queue for graph preprocessing
+	public HashMap<Integer,Node> map;				//returns Nodes by vertex (index)
+	public HashSet<Node> working;					//all nodes processed by query that must be reset
+	
+	public ArrayList<Barrier> barriers;				//for generating sets of inactive nodes for test graphs
+	
+	public final long INFINITY = Long.MAX_VALUE / 4;
+
+
+	
+	public int root;
+	public int cNode;
+	public int n;
+	public long mu = INFINITY;
+	public int processed;
+
+	
+	public BiGraph(int n) {
+		this.n = n;
+
+        this.graph = new ArrayList<ArrayList<Edge>>();
+        this.graphR = new ArrayList<ArrayList<Edge>>();
+        
+        this.heap = new ArrayList<Node>();
+        this.heapR = new ArrayList<Node>();
+        
+        this.preProc = new ArrayList<Node>();
+        
+        
+        
+        this.map = new HashMap<Integer,Node>(n+1);
+        
+        this.working = new HashSet<Node>();
+        
+        for (int i = 0; i <= n; i++) {					//graph indexes and nodes are 1 indexed, index 0 not used
+        	
+            this.graph.add(new ArrayList<Edge>());
+            this.graphR.add(new ArrayList<Edge>());          
+  
+        }
+        
+        this.root = -1;
+        this.cNode = -1;
+        
+        this.processed = 0;
+        
+        this.barriers = new ArrayList<Barrier>();
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	public Node addNode(int i) {
+		Node n = new Node(i);
+		map.put(i, n);
+		return n;
+	}
+	
+	public void initializeQueues() {
+		
+		heap.clear();
+		heapR.clear();
+		
+	}
+	
+	public void resetWorkingNodes() {
+		if(!working.isEmpty()) {
+			for(Node n: working) {
+				n.resetNode();
+			}
+			working.clear();
+		}
+		
+	}
+	
+	public void enQueue(Node n, ArrayList<Node> h) {
+		
+		int end = h.size();
+		h.add(end, n);
+		minSiftUp(end, h);
+//		working.add(n);
+		
+	}
+	
+	
+	public void addEdges(int s, int t, int c) {		//(source, target, length in 1 based indexing)
+
+		
+		Node source = map.get(s);
+		Node target = map.get(t);
+		
+		
+		Edge e = new Edge(source, target, c);
+        Edge er = new Edge(target, source, c);
+
+        graph.get(s).add(e);
+		graphR.get(t).add(er);
+
+	}
+	
+	
+	private void minSiftDown(int i, ArrayList<Node> h) {
+		
+//		System.out.println("minSiftDown 1: " + i);
+//		printHeap();
+		
+    	int mini = i;
+    	int li = h.size() - 1;	//0 based last index
+    	int left = 2*i + 1;
+    	int rght = 2*i + 2;
+    	
+    	if(h == heap) {
+    		if(left <= li && h.get(left).k < h.get(mini).k) {
+        		mini = left;
+        	}
+        	if(rght <= li && h.get(rght).k < h.get(mini).k) {
+        		mini = rght;
+        	}
+        	if(mini != i) {
+    	   		swap(i, mini, h);
+    	   		minSiftDown(mini, h);
+        	}
+    	} else {
+    		if(left <= li && h.get(left).kr < h.get(mini).kr) {
+        		mini = left;
+        	}
+        	if(rght <= li && h.get(rght).kr < h.get(mini).kr) {
+        		mini = rght;
+        	}
+        	if(mini != i) {
+    	   		swap(i, mini, h);
+    	   		minSiftDown(mini, h);
+        	}
+    	}
+ 
+    }
+	
+	private void printHeap(ArrayList<Node> h) {
+		System.out.print("heap: ");
+		for(Node n : h) {
+			System.out.print(n.index + ":" + n.dist + " ");
+		}
+		System.out.println(" ");
+	}
+	
+	
+	
+	private void minSiftUp(int i, ArrayList<Node> h) {
+		
+		int pi;
+//		System.out.println(" minSiftUp i: " + i);
+//		printHeap(h);
+		if(i > 0) {
+			pi = (i - 1)/2;
+			
+//			System.out.println(" minSiftUp pi: " + pi);
+			if(h == heap) {
+				if(h.get(pi).k > h.get(i).k) {
+					swap(pi, i, h);
+					minSiftUp(pi, h);
+				}
+			} else {
+				if(h.get(pi).kr > h.get(i).kr) {
+					swap(pi, i, h);
+					minSiftUp(pi, h);
+				}
+			}
+			
+			
+		}
+
+	}
+	
+	private Node getMin(ArrayList<Node> h) {
+		
+		Node nm = h.get(0);
+		swap(0, h.size() - 1, h);
+		h.remove(h.size() - 1);
+		minSiftDown(0, h);
+		
+		return nm;
+	}
+	
+	private void decreaseKey(Node dn, long d, Node start, Node finish, ArrayList<Node> h) {
+		
+//		System.out.println(" decreaseKey i: " + i + " d: " + d);
+		
+		if(h == heapR) {
+			dn.distR = d;
+			dn.setKr(start, finish);
+		} else {
+			dn.dist = d;
+			dn.setK(start, finish);
+		}
+		int dni = h.indexOf(dn);
+		minSiftUp(dni, h);
+	}
+	
+	private void decreaseKey(Node dn, long d, Node start, Node finish, ArrayList<Node> h, boolean astar) {
+		
+//		System.out.println(" decreaseKey i: " + i + " d: " + d);
+		
+		if(h == heapR) {
+			dn.distR = d;
+			if(astar)
+				dn.setKr(start, finish);
+			else
+				dn.setKr(start, finish, false);
+		} else {
+			dn.dist = d;
+			if(astar)
+				dn.setK(start, finish);
+			else
+				dn.setK(start, finish, false);
+		}
+		int dni = h.indexOf(dn);
+		minSiftUp(dni, h);
+	}
+	
+	private void decreaseKey(Node dn, long d, Node start, Node finish, ArrayList<Node> h, boolean astar, boolean biDirectional) {
+		
+//		System.out.println(" decreaseKey i: " + i + " d: " + d);
+		if(biDirectional) {
+			if(h == heapR) {
+				dn.distR = d;
+				if(astar)
+					dn.setKr(start, finish);
+				else
+					dn.setKr(start, finish, false);
+			} else {
+				dn.dist = d;
+				if(astar)
+					dn.setK(start, finish);
+				else
+					dn.setK(start, finish, false);
+			}
+			int dni = h.indexOf(dn);
+			minSiftUp(dni, h);
+		} else {
+			h = heap;							//if not bidirectional default to the forward direction
+			dn.dist = d;
+			if(astar)
+				dn.setK(start, finish);
+			else
+				dn.setK(start, finish, false);
+			int dni = h.indexOf(dn);
+			minSiftUp(dni, h);
+		}
+		
+	}
+
+    
+    private void swap(int i, int n, ArrayList<Node> h) {
+    	
+    	Node tn = h.get(i);
+    	h.set(i, h.get(n));
+    	h.set(n, tn);
+    	
+    }
+    
+    private long shortestPath() {
+    	long d = INFINITY;
+    	long dn = 0;
+    	for(Node n: working) {
+    		dn = n.dist + n.distR;
+    		d = Math.min(d, dn);
+    	}
+    	return d;					
+    }
+	
+	public long biAStar(int s, int t) {	//s & t are 0 indexed integers from the graph data
+		
+		//implements  bidirectional A* algorithm
+//		long start = System.nanoTime();	
+		processed = 0;
+		int dblProcessed = 0;
+		
+		double prt;
+		long mu = INFINITY;
+		
+		initializeQueues();
+		resetWorkingNodes();
+		
+		cNode = -1;
+		
+		Node sn = map.get(s);
+		Node tn = map.get(t);
+		
+		sn.dist = 0;
+		sn.setK(sn, tn);
+		sn.pindex = sn.index;
+		
+		tn.distR = 0;
+		tn.setKr(sn, tn);
+		tn.pindexR = tn.index;
+		
+		prt = tn.kr;									//reverse potential of target since tn.distr = 0;
+		
+		enQueue(sn, heap);
+		sn.queued = true;
+		enQueue(tn, heapR);
+		tn.queuedR = true;	
+		working.add(sn);								//add the initial nodes to the working set
+		working.add(tn);
+		
+		long result = -1;								//if after processing all nodes in the graph this is unchanged the target is unreachable
+		
+		
+		if(s == t) {
+			return 0;									//I found myself!!
+		}
+		
+		while(!heap.isEmpty() || !heapR.isEmpty()) {
+	
+			if(!heap.isEmpty()) {						//process the next node in the forward graph
+				
+				Node processing = getMin(heap);
+				processing.queued = false;
+				
+				
+//				System.out.println("processing Node: " + processing.index);
+				
+				for(Edge e : graph.get(processing.index)) {
+					
+					Node tt = e.v;	
+					
+					long td = processing.dist + e.length;
+				
+					if(tt.dist > td) {
+							
+							working.add(tt);
+							
+							if(tt.queued == false) {	
+								tt.dist = td;
+								tt.setK(sn, tn);
+								enQueue(tt, heap);
+								tt.queued = true;
+							} else {
+								decreaseKey(tt, td, sn, tn, heap);
+							}
+
+						tt.pindex = processing.index;				//min path is my daddy
+					}
+					
+				}
+				
+				processing.processed = true;
+				++processed;
+				
+				if(processing.processedR == true) {	
+					/*
+					long tp = processing.dist + processing.distR;
+					++dblProcessed;
+					if( tp < mu) {
+						mu = tp; 
+						result = mu;
+						cNode = processing.index;
+					}
+					if(!heap.isEmpty()) {
+						if(heap.get(0).k > mu && heapR.get(0).kr > mu) {
+							System.out.println("Exiting on break");
+							break;
+						}
+					}
+					 */
+					result = shortestPath();
+					break;
+										
+				}
+					
+			}
+			
+			if(!heapR.isEmpty()) {						//process the next node in the reverse graph
+				
+				Node processingR = getMin(heapR);
+				processingR.queuedR = false;
+				
+//				System.out.println("processing Node: " + rr.index);
+				
+				for(Edge er : graphR.get(processingR.index)) {
+			
+					Node ttr = er.v;		
+					
+					long tdr = processingR.distR + er.length;
+				
+					if(ttr.distR > tdr) {
+
+							working.add(ttr);
+							if(ttr.queuedR == false) {
+								ttr.distR = tdr;
+								ttr.setKr(sn, tn);
+								enQueue(ttr, heapR);
+								ttr.queuedR = true;
+							} else {
+								decreaseKey(ttr, tdr, sn, tn, heapR);
+							}
+					
+						ttr.pindexR = processingR.index;
+					}
+					
+				}
+				processingR.processedR = true;
+				++processed;
+				if(processingR.processed == true) {
+					/*
+					long tp = processingR.dist + processingR.distR;
+					++dblProcessed;
+					if( tp < mu) {
+						mu = tp;
+						result = mu;
+					}
+					if(!heapR.isEmpty()) {
+						if(heap.get(0).k > mu && heapR.get(0).kr > mu) {
+							break;
+						}
+					}
+					*/
+					result = shortestPath();
+					break;
+				}
+			}
+			
+		}
+//		long finish = System.nanoTime();
+//		long elapsed = (finish - start) / 1000000;
+//		System.out.println("BiAStar double processed nodes: " + dblProcessed);
+		return result;
+    }
+	
+	public long dijkstra(int s, int t) {
+		
+		//this is a checking method using the simple Dijkstra algorithm for testing
+		
+		
+		double prt = 0;									//in simple Dijkstra there is no potential
+		long mu = INFINITY;
+		processed = 0;
+		
+		initializeQueues();
+		resetWorkingNodes();
+		
+		cNode = -1;
+		
+		Node sn = map.get(s);
+		Node tn = map.get(t);
+		
+		sn.dist = 0;
+		sn.setK(sn, tn, false);							//false => don't use the A* potential in the K value
+	
+		enQueue(sn, heap);
+		sn.queued = true;
+		working.add(sn);								//add the initial nodes to the working set	
+		
+		if(s == t) {
+			return 0;									//I found myself!!
+		}
+		
+		
+	
+		while(!heap.isEmpty()) {						//process the next node in the forward graph
+			
+			Node processing = getMin(heap);
+			processing.queued = false;
+			
+//				System.out.println("processing Node: " + processing.index);
+			if(processing != tn) {
+				for(Edge e : graph.get(processing.index)) {
+					
+					Node tt = e.v;	
+					
+					long td = processing.dist + e.length;
+				
+					if(tt.dist > td) {
+							
+							working.add(tt);
+							
+							if(tt.queued == false) {	
+								tt.dist = td;
+								tt.setK(sn, tn, false);
+								enQueue(tt, heap);
+								tt.queued = true;
+							} else {
+								decreaseKey(tt, td, sn, tn, heap, false, false);		//use the unidirectional non potential version of the method
+							}
+
+//							tt.pindex = processing.index;				//min path is my daddy
+					}
+						
+				}
+				
+				processing.processed = true;
+				++processed;
+				
+			} else {							//processing the target node here
+				
+				if(processing.dist < mu)
+					mu = processing.dist;
+				processing.processed = true;
+			}
+		
+			
+			
+			if(tn.processed == true) {				
+				if(!heap.isEmpty()) {
+					if(heap.get(0).k > mu) {	//stop when the shortest queued node distance is longer than the shortest path found
+						break;
+					}
+				}
+			}				
+		}
+//		System.out.println("Dijkstra processed edges: " + processed);
+		return mu == INFINITY? -1: mu;
+
+	}
 }
 
 
